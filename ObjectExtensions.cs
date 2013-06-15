29c9f91debff74abbe750e68a072cea904eef738
Burtsev Aleksey <bpipe@mail.ru>
2013-06-15 16:04:57 +0400

    Fixed cloning zero length arrays of reference types.

--- ObjectExtensions.cs
+++ ObjectExtensions.cs
@@ -86,6 +86,7 @@ namespace System
         {
             public static void ForEach(this Array array, Action<Array, int[]> action)
             {
+                if (array.LongLength == 0) return;
                 ArrayTraverse walker = new ArrayTraverse(array);
                 do action(array, walker.Position);
                 while (walker.Step());
